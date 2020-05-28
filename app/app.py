fa0ad8b53af8fe4008b6843bbd36fc436e8677d7
Benoit Bovy <benbovy@gmail.com>
2020-05-28 12:10:26 +0200

    extract tracks: fix json response, >1 fieldnames

--- app/app.py
+++ app/app.py
@@ -5,15 +5,27 @@
 # $ export FLASK_APP=api_demo.py
 # $ python -m flask run --host=0.0.0.0 --port=9000
 
-from flask import Flask, request
+from flask import Flask, jsonify, request
+from flask.json import JSONEncoder
 from fsspec.implementations.http import HTTPFileSystem
+import numpy as np
 import requests
 import xarray as xr
 
-from utils import extract_field_along_tracks
+from utils import extract_data_along_tracks
+
+
+# https://stackoverflow.com/a/47626762
+class NumpyEncoder(JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
 
 
 app = Flask(__name__)
+app.json_encoder = NumpyEncoder
 
 # url and port of xpublish server running for the demo dataset
 demo_dataset_server_url = "http://xpublish:9000"
@@ -51,15 +63,16 @@ def extract_demo_data_along_tracks():
     """Extract data along given ship tracks."""
 
     request_data = request.get_json()
-
     dataset = xr.open_zarr(demo_dataset_http_map, consolidated=True)
 
-    transform = request_data["transform"]
+    transform = request_data.get("transform")
 
     if transform is not None:
         if transform.get("aggregation") == "mean":
             dataset = dataset.mean(dim=transform.get("dim"))
 
-    return extract_field_along_tracks(
-        dataset, request_data["fieldname"], request_data["tracks"]
+    extract_data = extract_data_along_tracks(
+        dataset, request_data["fieldnames"], request_data["tracks"]
     )
+
+    return jsonify(data=extract_data)
